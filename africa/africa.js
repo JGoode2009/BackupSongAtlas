d79018b8e9a48ed7bafa9ddecde576eb5a8926df
JGoode2009 <105678312+JGoode2009@users.noreply.github.com>
2023-07-07 23:32:14 -0500

    Create africa.js

---
+++ africa/africa.js
@@ -0,0 +1,8 @@
+const songList =[{
+    songName:"Only You" ,
+    artistName: ["Steve Monite"] ,
+    songFile: <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/0emBnGnUN9E3jT9gSIkr7w?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>,
+    genres: ["Afro Boogie", "Techno Disco", "Disco"],
+    country: ["Nigeria"],
+    continent: ["South America"]
+}]
